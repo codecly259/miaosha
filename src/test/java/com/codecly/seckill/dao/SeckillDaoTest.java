92bfb1c113a57065889cd3e00d2c07faaa64ac91
codecly259 <maxinchun0215@qq.com>
2016-05-24 10:30:56 +0800

    修改项目名称

---
+++ src/test/java/com/codecly/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,24 @@
+package com.codecly.seckill.dao;
+
+import org.junit.Test;
+
+/**
+ * Created by maxinchun on 2016/5/23.
+ */
+public class SeckillDaoTest {
+
+    @Test
+    public void testReduceNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testQueryById() throws Exception {
+
+    }
+
+    @Test
+    public void testQueryAll() throws Exception {
+
+    }
+}
